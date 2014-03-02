3c7a2e04a51bb2a975ec26cb07ac19094efcc10c
SirTriple <resistance.fom@hotmail.com>
2014-03-02 14:59:19 +1030

    Fixed inequality
    Made teams fair, made it so you can't choose your team, you just get 1 assigned.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,6 +3,8 @@ AddCSLuaFile( "shared.lua" )
  
 include( 'shared.lua' )
 
+// Some gamemode base settings
+
 function GM:PlayerSpawn( ply )
     self.BaseClass:PlayerSpawn( ply )   
  
@@ -14,13 +16,28 @@ function GM:PlayerSpawn( ply )
  
 end
 
+// What you get when you spawn
+
 function GM:PlayerLoadout( ply )
 
 	ply:SetArmor( 5 )
-	ply:Give( "Dubstep Cannon" )
+	
+// Making teams fair
+ 
+local team = math.random( 1, 2 )
+	if team = 1 then
+		ply:SetTeam( 1 )
+	elseif
+		team = 2 then 
+			ply:SetTeam( 2 )
+		end
+	end
 	
 end
 
 function GM:PlayerInitialSpawn( ply )
 	   ply:PrintMessage( HUD_PRINTTALK, "Welcome, " .. ply:Name() .. "to Dubstep WarFare, a gamemode by Triple and Boss" )
 end
+
+
+
