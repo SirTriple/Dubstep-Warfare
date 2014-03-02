24a740cbee8c1d85072b51f2cf13f638a7574608
SirTriple <resistance.fom@hotmail.com>
2014-03-02 14:48:09 +1030

    Added teams
    Beats are red and Rhythm and blue.

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -4,3 +4,9 @@ GM.Email = "N/A"
 GM.Website = "http://steamcommunity.com/id/triplef/"
 
 DeriveGamemode( "sandbox" )
+
+// The teams of the gamemode
+
+team.SetUp( 1, "Beats", Color( 255, 0, 0))
+team.SetUp( 2, "Rhythm", Color( 0, 0, 255))
+team.SetUp( 3, "Spectators", Color( 0, 255, 0))
